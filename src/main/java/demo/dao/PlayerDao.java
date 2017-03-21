9c4cb8a762b9cddf83ea27722dc5b48c3ebf7bff
Andrey Shalunov <shalunov@netcracker.com>
2017-03-21 08:04:45 +0400

    Add players

--- src/main/java/demo/dao/PlayerDao.java
+++ src/main/java/demo/dao/PlayerDao.java
@@ -5,13 +5,10 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import demo.domain.Player;
 import demo.domain.Team;
 
-@RestResource(path="teams", rel="teams")
-public interface TeamDao extends CrudRepository<Team, Long>{
+@RestResource(path="players", rel="players")
+public interface PlayerDao extends CrudRepository<Player, Long>{
 	
-	List<Team> findAll();
-	
-	Team findByName(String name);
-
 }
